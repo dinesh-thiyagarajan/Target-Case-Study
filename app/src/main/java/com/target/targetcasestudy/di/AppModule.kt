5004ee31bf507a648b04513843a2c08d9e06bccb
Dinesh <dineshkarthie@gmail.com>
2021-04-12 13:11:47 +0530

    Dependency Modules added for Retrofit, OkHttp and Gson

--- app/src/main/java/com/target/targetcasestudy/di/AppModule.kt
+++ app/src/main/java/com/target/targetcasestudy/di/AppModule.kt
@@ -1,4 +1,49 @@
 package com.target.targetcasestudy.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.target.targetcasestudy.helpers.AppConstants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @[Provides Singleton]
+    fun providesGson(): Gson {
+        return GsonBuilder()
+            .setLenient()
+            .create()
+    }
+
+    @[Provides Singleton]
+    fun providesOkHttp(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+        httpClient.callTimeout(5, TimeUnit.SECONDS)
+        httpClient.connectTimeout(5, TimeUnit.SECONDS)
+        return httpClient.build()
+    }
+
+    @[Provides Singleton]
+    fun providesRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(AppConstants.BASE_URL)
+            .client(okHttpClient)
+            .build()
+    }
+
 }
---
+++ app/src/main/java/com/target/targetcasestudy/helpers/AppConstants.kt
@@ -0,0 +1,7 @@
+package com.target.targetcasestudy.helpers
+
+object AppConstants {
+
+    const val BASE_URL = "https://api.target.com/mobile_case_study_deals/v1"
+
+}
